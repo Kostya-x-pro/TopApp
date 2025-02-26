51d9dd1d3b16a750e30b6b98cce7a35b4528a57c
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2025-02-26 23:26:51 +0300

    подключение шрифтов в NextJS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { JSX } from "react";
 import Head from "next/head";
 
@@ -12,6 +13,10 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
           <title>MyTop App</title>
           <link  rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100..900&display=swap" rel="stylesheet"/>
+          
       </Head>
       <Component {...pageProps} />
     </>
