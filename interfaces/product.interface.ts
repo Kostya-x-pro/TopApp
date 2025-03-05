97d0d913b41abcb0957ccd4c348b6549748c22bf
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2025-03-05 14:05:58 +0300

    Получение данных от сервера о страницах и продуктах, использование getStaticPaths

---
+++ interfaces/product.interface.ts
@@ -0,0 +1,46 @@
+export interface ProductCharacteristic {
+    name: string;
+    value: string;
+  }
+
+export interface ReviewModel {
+_id: string;
+name: string;
+title: string;
+description: string;
+rating: number;
+createdAt: Date;
+}
+
+export interface ProductModel {
+    _id: string;
+    categories: string[];
+    tags: string[];
+    title: string;
+    image: string;
+    description: string;
+    link: string;
+    price: number;
+    credit: number;
+    oldPrice: number;
+    characteristics: ProductCharacteristic[];
+    advantages?: string;
+    initialRating: number;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+
+    reviews: ReviewModel[];
+    reviewCount: number;
+    reviewAvg?: number;
+}
+  
+
+  
+
+  
+  export interface Blog {
+    text: string;
+    _id: string;
+    bigImage?: string;
+  }
