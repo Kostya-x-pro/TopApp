91c920d98db4737d7b8ce3efa9505ea9e2173945
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2025-03-05 12:14:04 +0300

    Получены данные через getStaticProps и добавлена билблиотека axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import { JSX, useState } from "react";
+import { GetStaticProps } from "next";
+import axios from 'axios';
 
 // HOC (компоненты)
 import { withLayout } from "@/layout/withLayout";
 
 // Компоненты
 import { Button, Htag, Paragraph, Tag, Rating } from "@/components";
+import { MenuItem } from "@/interfaces/menu.interface";
 
- function Home(): JSX.Element {
+ function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
-
 
   return (
     <>
@@ -24,8 +26,36 @@ import { Button, Htag, Paragraph, Tag, Rating } from "@/components";
       <Tag size="m" color="green">Сред</Tag>
       <Tag color="primary">Pirmary</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+      {menu.map(m => (
+        <li 
+          key={m._id.secondCategory}
+          >
+            {m._id.secondCategory}
+        </li>
+      ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + 'api/top-page/find', {
+    firstCategory
+  } );
+
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[],
+  firstCategory: number
+}
