ad66684d5043e281d534a89454bf081f989d0112
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2025-03-05 20:25:41 +0300

    добавлен компонент контекста в проект

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -4,13 +4,14 @@ import { SidebarProps } from "./Sidebar.props";
 
 import styles from './Sidebar.module.css';
 import cn from "classnames";
+import { Menu } from "../Menu/Menu";
 
 export const Sidebar = (props: SidebarProps): JSX.Element => {
     const { ...otherProps } = props;
 
     return (
         <div {...otherProps}>
-            Sidebar
+            <Menu />
         </div>
     );
 };
--- layout/withLayout.tsx
+++ layout/withLayout.tsx
@@ -1,12 +1,17 @@
 import { FC, JSX } from "react";
+import { AppContextProvider, IAppContext } from "@/context/app.context";
 import { Layout } from "./Layout";
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FC<T>) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FC<T>) => {
     return function withLayoutComponent(props: T): JSX.Element {
+      const { menu, firstCategory } = props;
+
       return (
-        <Layout>
-          <Component {...props}/>
-        </Layout>
+        <AppContextProvider menu={menu} firstCategory={firstCategory}>
+          <Layout>
+            <Component {...props}/>
+          </Layout>
+        </AppContextProvider>
       );
     };
   };
