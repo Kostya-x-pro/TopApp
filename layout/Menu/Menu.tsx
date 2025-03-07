b371d4bcbe45c507bec9fff94715526942dbd8d8
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2025-03-07 10:30:08 +0300

    добавлен роутинг по меню сайдбара

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,4 +1,6 @@
 import { JSX, useContext } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { AppContext } from "@/context/app.context";
 
 import { FirstLevelMenuItem, PageItem } from "@/interfaces/menu.interface";
@@ -21,20 +23,32 @@ const firstLevelMenu: FirstLevelMenuItem[] = [
 
 export const Menu = (): JSX.Element => {
     const { menu, setMenu, firstCategory } = useContext(AppContext);
+    const router = useRouter();
 
+    const openSecondLevel = (secondCategory: string):void => {
+        if (setMenu) {
+            setMenu(menu.map(m => {
+                if (m._id.secondCategory === secondCategory) {
+                    m.isOpened = !m.isOpened;
+                }
+                return m;
+            }));
+        }
+    };
+    
     const buildFirstLevel = (): JSX.Element => {
         return(
             <>
             {firstLevelMenu.map(m => (
                 <div key={m.route}>
-                    <a href={`/${m.route}`}>
+                    <Link href={`/${m.route}`}>
                         <div className={cn(styles.firstLevel, {
                             [styles.firstLevelActive]: m.id === firstCategory
                         })}>
                             {m.icon}
                             <span>{m.name}</span>
                         </div>
-                    </a>
+                    </Link>
                     {m.id === firstCategory && buildSecondLevel(m)}
                 </div>
             ))}
@@ -45,16 +59,27 @@ export const Menu = (): JSX.Element => {
     const buildSecondLevel = (menuItem: FirstLevelMenuItem): JSX.Element => {
         return (
             <div className={styles.secondBlock}>
-                {menu.map(m => (
-                    <div key={m._id.secondCategory}>
-                        <div className={styles.secondLevel}>{m._id.secondCategory}</div>
-                        <div className={cn(styles.secondLevelBlock, {
-                            [styles.secondLevelBlockOpend]: m.isOpened
-                        })}>
-                            {buildThirdLevel(m.pages, menuItem.route)}
+                {menu.map(m => {
+
+                    if (m.pages.map(p => p.alias).includes(router.asPath.split('/')[2])) {
+                        m.isOpened = true;
+                    }
+
+                    return (
+                        <div key={m._id.secondCategory}>
+                            <div 
+                                className={styles.secondLevel}
+                                onClick={() => openSecondLevel(m._id.secondCategory)}>
+                                {m._id.secondCategory}
+                            </div>
+                            <div className={cn(styles.secondLevelBlock, {
+                                [styles.secondLevelBlockOpend]: m.isOpened
+                            })}>
+                                {buildThirdLevel(m.pages, menuItem.route)}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
         );
     };
@@ -63,14 +88,15 @@ export const Menu = (): JSX.Element => {
         return (
             <>
                 {pages.map(p => (
-                    <a 
-                        key={p._id} 
-                        href={`/${route}/${p.alias}`} 
-                        className={cn(styles.thirdLevel, {
-                        [styles.thirdLevelActive]: false
-                    })}>
+                    <Link 
+                    href={`/${route}/${p.alias}` }
+                    key={p._id}
+                    className={cn(styles.thirdLevel, {
+                        [styles.thirdLevelActive]: `/${route}/${p.alias}` === router.asPath
+                    })}
+                    >
                         {p.category}
-                    </a>
+                </Link>
                 ))}
             </>
         );
