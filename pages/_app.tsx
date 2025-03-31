2d8113067f928c80ce462a58cb2bbefb0bdc82b9
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2025-03-31 15:42:34 +0300

    Подключена яндекс метрика

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,32 @@
 /* eslint-disable @next/next/no-page-custom-font */
-import { JSX } from "react";
+import { JSX, useEffect } from "react";
 import Head from "next/head";
+import ym, { YMInitializer } from "react-yandex-metrika";
 
 import type { AppProps } from "next/app";
 
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
 
-export default function App({ Component, pageProps, router }: AppProps): JSX.Element {
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
   
+// При подключении метрики, показатили LightHouse упадут
+  useEffect(() => {
+    const handleRouteChange = (url: string): void => {
+      if (typeof window !== "undefined") {
+        ym("hit", url);
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return (): void => {
+
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+
   return (
     <>
       <Head>
@@ -15,11 +34,17 @@ export default function App({ Component, pageProps, router }: AppProps): JSX.Ele
           <link  rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+          <link rel="preconnect" href="https://mc.yandex.ru"/>
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100..900&display=swap" rel="stylesheet"/>
           <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMIN + router.asPath} />
           <meta property="og:locale" content="ru_RU" />
           
       </Head>
+      <YMInitializer 
+        accounts={[]}
+        options={{ webvisor: true, defer: true }}
+        version="2"
+      />
       <Component {...pageProps} />
     </>
   );
